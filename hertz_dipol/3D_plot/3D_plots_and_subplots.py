10eea05b65bb76036f7d2a629bc233d84bcc845e
Angelus Vincent <angelus.vincent@campus.tu-berlin.de>
2024-06-13 17:26:28 +0200

    working 3D plot

--- hertz_dipol/3D_plot/3D_plots_and_subplots.py
+++ hertz_dipol/3D_plot/3D_plots_and_subplots.py
@@ -5,27 +5,29 @@ from matplotlib import cm
 from mpl_toolkits.mplot3d.axes3d import get_test_data
 
 # set up a figure twice as wide as it is tall
-fig, ax = plt.subplots(figsize=(5, 5), subplot_kw={'projection': '3d'})
+fig, ax = plt.subplots(figsize=(10, 10), subplot_kw={'projection': '3d'})
 
 # =============
 # First subplot
 # =============
 # set up the Axes for the first plot
 
-def Voltage(theta, r):
-    return np.sin(theta)**2 / r**2
-
+def Voltage(x, y):
+    return np.sin(y)**2 / x
 # plot a 3D surface like in the example mplot3d/surface3d_demo
 theta = np.arange(0, 2*np.pi, 0.01) 
-X = np.linspace(-30, 30, num=200)
-Y = np.linspace(-30, 30, num=200)
+X = np.linspace(1, 30, num=200)
+Y = np.linspace(0, np.pi, num=200)
 X, Y = np.meshgrid(X, Y)
-R = np.sqrt(X**2 + Y**2)
-Z = np.array([np.sin(angel)**2 / radius**2 for angel, radius in zip(theta, R)])
+Z = Voltage(X, Y)
+
 surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
 #ax.set_zlim(min(Z), max(Z))
 fig.colorbar(surf, shrink=0.5, aspect=10)
+ax.set_xlabel('Abstand r')
+ax.set_ylabel(r'Winkel $\theta$')
+ax.set_zlabel('Spannung U')
 
 # ==============
 # Second subplot
@@ -36,5 +38,5 @@ fig.colorbar(surf, shrink=0.5, aspect=10)
 # plot a 3D wireframe like in the example mplot3d/wire3d_demo
 #X, Y, Z = get_test_data(0.05)
 #ax.plot_wireframe(X, Y, Z, rstride=10, cstride=10)
-
+#plt.savefig('3D.jpg')
 plt.show()
